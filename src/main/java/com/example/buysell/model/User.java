038d330cf1c0d06be47313c2212fc96f54c529df
Ol Ua <rudojsua@gmail.com>
2023-05-04 23:02:51 +0200

    Spring security 1

---
+++ src/main/java/com/example/buysell/model/User.java
@@ -0,0 +1,83 @@
+package com.example.buysell.model;
+
+import com.example.buysell.model.enums.Role;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "email", unique = true)
+
+    private String email;
+    @Column(name = "phone_number")
+
+    private String phoneNumber;
+    @Column(name = "name")
+
+    private String name;
+    @Column(name = "active")
+
+    private boolean active;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "image_id")
+    private Image avatar;
+    @Column(name = "password", length = 1000)
+
+    private String password;
+    @Column(name = "roles")
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns =@JoinColumn("user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
+    @Column(name = "dateOfCreated")
+
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
